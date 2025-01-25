50fa0e8ec9880b1940d43521dfea5cef68194fdd
apparentorder <github@neveragain.de>
2025-01-25 09:08:21 +0100

    fix (and simplify) generating "changes"

--- awsipv6-update.sh
+++ awsipv6-update.sh
@@ -24,35 +24,27 @@ fi
 
 python3 awsipv6-get.py "$BOTOCORE_REPO" $LIVE_ARG
 
-changes="output/changes"
+changes_output="output/changes"
+changes_prev=$(mktemp /tmp/awsipv6-changes_prev.XXXXXX)
 changes_today=$(mktemp /tmp/awsipv6-changes_today.XXXXXX)
-text_prev=$(mktemp /tmp/awsipv6-text_prev.XXXXXX)
-trap "rm -f $changes_today $text_prev" INT TERM EXIT
+endpoints_text_prev=$(mktemp /tmp/awsipv6-text_prev.XXXXXX)
+trap "rm -f $changes_today $changes_prev $text_prev" INT TERM EXIT
 
 today=$(date +%Y-%m-%d)
 
-aws s3 cp "$S3BASE"/changes "$changes"
-aws s3 cp "$S3BASE"/endpoints.text "$text_prev"
+aws s3 cp "$S3BASE"/changes "$changes_prev"
+aws s3 cp "$S3BASE"/endpoints.text "$endpoints_text_prev"
 
-diff -wu0 \
-    "$text_prev" \
-    output/endpoints.text \
+diff -wu0 "$endpoints_text_prev" output/endpoints.text \
+    | sed 1,2d \
     | grep -v 'amazonwebservices.com.cn ' \
+    | grep '^[-+]' \
     > "$changes_today" \
-|| true
+    || true
 
 if test -s "$changes_today"; then
-    # write complete history to changes_today, beginning with today's changes
-    echo "$today" > "$changes_today"
-    (
-        diff -wu0 "$text_prev" output/endpoints.text \
-        | sed 1,2d \
-        | grep '^[-+]'
-    ) >> "$changes_today"
-    cat "$changes" >> "$changes_today"
-    mv "$changes_today" "$changes"
-    
-    aws s3 cp "$changes" "$S3BASE"/changes
+    echo "$today" > "$changes_output"
+    cat "$changes_today" "$changes_prev" >> "$changes_output"
 fi
 
 python3 awsipv6-html.py "$BOTOCORE_REPO" $LIVE_ARG > output/endpoints.html
