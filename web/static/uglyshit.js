511b53bcd3c03889c728a629b4e088e22e02fba4
apparentorder <github@neveragain.de>
2025-10-15 19:37:28 +0200

    .

--- web/static/uglyshit.js
+++ web/static/uglyshit.js
@@ -1,34 +1,4 @@
 
-function filterRegions() {
-    const filter = document.getElementById('region-search').value.toLowerCase();
-    const checkboxLabels = document.getElementById('region-checkboxes').getElementsByTagName('label');
-    for (const cl of checkboxLabels) {
-        cl.style.display = cl.textContent.toLowerCase().includes(filter) ? '' : 'none';
-    }
-}
-
-function removeEndpointClassFilter() {
-    // ... but only if it was set by default and not by the user.
-    const defaultInput = document.querySelector('input[name="filter-class"][checked="default"]');
-
-    if (defaultInput) {
-        // The "checked" *attribute* can be false (not checked) but the *attribute value*
-        // could still be "default" after the user has selected another filter class option.
-        if (defaultInput.checked) {
-            document.getElementById('class-all').checked = true;
-        }
-
-        defaultInput.removeAttribute('checked');
-    }
-}
-
-function setAllRegionsChecked(isChecked) {
-    for (const i of document.getElementsByName('regions')) {
-        i.checked = isChecked;
-        i.dispatchEvent(new Event('change'));
-    }
-}
-
 let tooltipElement = null;
 let currentTooltipUrl = null;
 
@@ -83,6 +53,9 @@ document.addEventListener('mouseenter', function(e) {
     if (isFirstCell || isProgressBar) {
         const serviceName = tr.querySelector('td').textContent.trim();
         showTooltip(tr, serviceName);
+
+        if (this.serviceToolTipTimer) clearTimeout(this.serviceToolTipTimer);
+        this.serviceToolTipTimer = setTimeout(hideTooltip, 10_000);
     }
 }, true);
 
@@ -100,8 +73,9 @@ document.addEventListener('touchstart', function(e) {
     if (isFirstCell || isProgressBar) {
         const serviceName = tr.querySelector('td').textContent.trim();
         showTooltip(tr, serviceName);
-        // Hide after 6 seconds or on next touch
-        // setTimeout(hideTooltip, 6000);
+
+        if (this.serviceToolTipTimer) clearTimeout(this.serviceToolTipTimer);
+        this.serviceToolTipTimer = setTimeout(hideTooltip, 10_000);
     }
 }, true);
 
